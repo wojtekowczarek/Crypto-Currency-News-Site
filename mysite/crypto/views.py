11768559f226c3430aa62133ac538f5b66b6f1fc
Wojtek Owczarek <wojciech.owczarek92@gmail.com>
2019-11-25 12:43:35 +0100

    added Currency Price information by another request to API in home function and displayed it on landing page

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def home(request):
 	import requests
 	import json
+
+	# Grab Crypto Price Data
+	price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD,EUR,JPY,PLN")
+	price = json.loads(price_request.content)
+
+	# Grab Crypto News from external API
 	api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	api = json.loads(api_request.content)
-	return render(request, 'home.html', {'api': api})
+	return render(request, 'home.html', {'api': api, 'price': price})
