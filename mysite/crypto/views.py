d34c05a4e441fca9702877fde7add9e3171c7580
Wojtek Owczarek <wojciech.owczarek92@gmail.com>
2019-11-25 14:25:47 +0100

    added function to deal with Searching form, handle incorrect inputs and displays searched currencies, added new url path and styling

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -6,10 +6,25 @@ def home(request):
 	import json
 
 	# Grab Crypto Price Data
-	price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD,EUR,JPY,PLN")
+	price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD")
 	price = json.loads(price_request.content)
 
 	# Grab Crypto News from external API
 	api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	api = json.loads(api_request.content)
 	return render(request, 'home.html', {'api': api, 'price': price})
+
+
+def prices(request):
+	if request.method == 'POST':
+		import requests
+		import json
+
+		quote = request.POST['quote']
+		quote = quote.upper()
+		crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+		crypto = json.loads(crypto_request.content)
+		return render(request, 'prices.html', {'quote': quote, 'crypto': crypto})
+	else:
+		notfound = "Enter a correct crypto currency symbol into the form above..."
+		return render(request, 'prices.html', {'notfound': notfound})
